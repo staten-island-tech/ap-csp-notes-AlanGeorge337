88239ef430953a61565ad25d915afbbce29db8b7
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2020-09-24 10:02:19 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const myAge = 17;
+
+const yourAge = 21;
+
+console.log(myAge + yourAge);
+
+
+
+
+
+console.log('Hello my name is' + ' Alan');
+
+
+
+console.log("Hello it's me Alan");
+
+
+
+
+const name = "Alan";
+const myAge = 17; 
+
+console.log(`Hello it's me and my name is ${name}`)
+
+
+console.log("Hello it's me" + name + " and my name is" + age + " and I'm getting old.");
+
+console.log(`Hello it's me ${name} and my age is ${myAge}`); 
+
+const combined = name + myAge;
+
+console.log(typeof combined); 
