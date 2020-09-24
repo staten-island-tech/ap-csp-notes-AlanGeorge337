924613d0db767a6a27be21acde0d214ce79254fc
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2020-09-24 10:14:22 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const name = 'Alan';
+cosnt youtuber = 'Traversy Media'; 
+
+fucntion adder(num1, num2) {
+    console.log(num1 + num2);
+}
+
+adder(5, 10);
+
+
+
+function toUpper(text){
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
+
+
+toUpper(name);
+toUpper(youtuber);
+
+
+
+const toUpper = () =>{
+
+}
+
